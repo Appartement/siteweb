3be5e8ca2bfac1eb3e57f83b3de0deda180dc9b7
Ali-Minejem <minejem.ali@gmail.com>
2025-04-26 00:25:16 +0100

    fixing services card text size

--- src/pages/services/ServiceTransaction.tsx
+++ src/pages/services/ServiceTransaction.tsx
@@ -55,7 +55,6 @@ const ServiceTransaction = () => {
                     <h3 className="text-xl lg:text-3xl font-light text-white mb-4 drop-shadow-md">
                       {t("servicesPage.transaction.buy.title")}
                     </h3>
-                    <div className="hidden"></div>
                     <p className="text-white font-light drop-shadow-md">
                       {t("servicesPage.transaction.buy.subtitle")}
                     </p>
